1845eb7bd3bd144906fe4c73aa77032491bc1342
Dinesh Kumawat <dkumawat7627@gmail.com>
2025-03-22 19:24:45 +0530

    Add files via upload

---
+++ src/test/java/com/amazon/aws/sqs/AwsSqsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amazon.aws.sqs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsSqsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
